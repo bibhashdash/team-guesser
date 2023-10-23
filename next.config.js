5a886e65d84960253465b513c134f3ee4938158c
bibhashdash <b.dash@12os.co.uk>
2023-10-23 20:02:43 +0100

    Attempting to hide env vars

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        API_KEY: process.env.FWP_API_KEY,
+    }
+}
 
 module.exports = nextConfig
--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,14 +1,17 @@
 const url = 'https://football-web-pages1.p.rapidapi.com/teams.json?comp=1';
+const apiKey = process.env.API_KEY;
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '7e3e7507fbmsh3ac2e6ed9ad32bep1d3fa3jsn12b698d56994',
+    'X-RapidAPI-Key': apiKey,
     'X-RapidAPI-Host': 'football-web-pages1.p.rapidapi.com'
   }
 };
 
 export const getTeams = async () => {
+
   try {
+
     const response = await fetch(url, options);
     const data = await response.json();
     if (response.ok) { // if HTTP-status is 200-299
