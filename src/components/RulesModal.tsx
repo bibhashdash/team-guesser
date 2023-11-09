260bf735c0bb462e3d7c9165c4058ad02da66729
bibhashdash <b.dash@12os.co.uk>
2023-11-09 22:32:08 +0000

    wip...

--- src/components/RulesModal.tsx
+++ src/components/RulesModal.tsx
@@ -7,6 +7,7 @@ import rule1Pic1 from "../../public/assets/rule1pic1.png";
 import {WhiteSquaresContainer} from "@/components/WhiteSquaresContainer";
 import {GameResult, GameState, InputTab} from "@/utlities/models";
 import {InputSection} from "@/components/InputSection";
+import {Keyboard} from "@/components/Keyboard";
 
 interface RulesModalProps {
   onClickClose: () => void;
@@ -22,7 +23,8 @@ export const RulesModal = ({onClickClose}: RulesModalProps) => {
       <div className="text-xs">
         <Carousel autoplay={false}>
           <RulesModalSlide index={1}>
-            <p className="leading-6 text-left"><span className="font-display">?ERFECT</span> is a really simple game (and very
+            <p className="leading-6 text-left"><span className="font-display">?ERFECT</span> is a really simple game
+              (and very
               similar to Hangman). You can enter characters one by one and see how many occurrences there are in
               the answer.</p>
             <div className="w-full flex flex-col items-center border-2 border-gray50 rounded-md p-2">
@@ -31,14 +33,16 @@ export const RulesModal = ({onClickClose}: RulesModalProps) => {
               <InputSection inputTab={InputTab.oneByOne} userInput="e" userNuclearInput="" onClickTab={() => {
               }}/>
             </div>
-            <p className="leading-6 text-left">You can keep going till you get the full name. If the character isn't in the name, it won't show up in the answer board.</p>
+            <p className="leading-6 text-left">You can keep going till you get the full name. If the character isn't in
+              the name, it won't show up in the answer board.</p>
           </RulesModalSlide>
           <RulesModalSlide index={2}>
             <p className="leading-6 text-left">Or you can chance it in one go. Do you dare?</p>
             <div className="w-full flex flex-col items-center border-2 border-gray50 rounded-md p-2">
               <WhiteSquaresContainer matcherText="Arsenal" userSubmissionArray={["f", "r", "s", "e", "n", "l"]}
                                      gameState={GameState.gameOver} gameResult={GameResult.loss}/>
-              <InputSection inputTab={InputTab.goForGlory} userInput="e" userNuclearInput={["f", "r", "s", "e", "n", "a", "l"]} onClickTab={() => {
+              <InputSection inputTab={InputTab.goForGlory} userInput="e"
+                            userNuclearInput={["f", "r", "s", "e", "n", "a", "l"]} onClickTab={() => {
               }}/>
             </div>
             <p className="leading-6 text-left">The downside? If you get it wrong, you lose. No re-dos!</p>
@@ -46,32 +50,57 @@ export const RulesModal = ({onClickClose}: RulesModalProps) => {
           <RulesModalSlide index={3}>
             <p className="leading-6 text-left">Numbers/Dashes/Ampersands are very much possible in a team name.</p>
             <div className="w-full flex flex-col items-center border-2 border-gray50 rounded-md p-2">
-              <WhiteSquaresContainer matcherText="Vfl Bochum 1848" userSubmissionArray={['v', 'f', 'l', 'b', 'o', 'c', 'h', 'u', 'm', '1', '8', '4', '8']}
+              <WhiteSquaresContainer matcherText="Vfl Bochum 1848"
+                                     userSubmissionArray={['v', 'f', 'l', 'b', 'o', 'c', 'h', 'u', 'm', '1', '8', '4', '8']}
                                      gameState={GameState.gameOver} gameResult={GameResult.win}/>
-              <InputSection inputTab={InputTab.goForGlory} userInput="e" userNuclearInput={['V', 'f', 'l', ' ', 'B', 'o', 'c', 'h', 'u', 'm', ' ', '1', '8', '4', '8']} onClickTab={() => {
-              }}/>
+              <InputSection inputTab={InputTab.goForGlory} userInput="e"
+                            userNuclearInput={['V', 'f', 'l', ' ', 'B', 'o', 'c', 'h', 'u', 'm', ' ', '1', '8', '4', '8']}
+                            onClickTab={() => {
+                            }}/>
             </div>
             <p className="leading-6 text-left">Other special characters, not really!</p>
           </RulesModalSlide>
           <RulesModalSlide index={4}>
-            <p className="leading-6 text-left">In <span className="text-blue300 font-bold">One by One</span> mode if you submit a character, it is no longer available on the keyboard.</p>
+            <p className="leading-6 text-left">In <span className="text-blue300 font-bold">One by One</span> mode once you
+              submit a character, it is no longer available on the keyboard.</p>
             <div className="w-full flex flex-col items-center border-2 border-gray50 rounded-md p-2">
-              <WhiteSquaresContainer matcherText="Vfl Bochum 1848" userSubmissionArray={['v', 'f', 'l', 'b', 'o', 'c', 'h', 'u', 'm', '1', '8', '4', '8']}
-                                     gameState={GameState.gameOver} gameResult={GameResult.win}/>
-              <InputSection inputTab={InputTab.goForGlory} userInput="e" userNuclearInput={['V', 'f', 'l', ' ', 'B', 'o', 'c', 'h', 'u', 'm', ' ', '1', '8', '4', '8']} onClickTab={() => {
-              }}/>
+              <WhiteSquaresContainer matcherText="Chelsea"
+                                     userSubmissionArray={['e', 'l', 's']}
+                                     gameState={GameState.gameStarted} gameResult={GameResult.default}/>
+              <InputSection inputTab={InputTab.oneByOne} userInput=""
+                            userNuclearInput={[]}
+                            onClickTab={() => {
+                            }}/>
+              <Keyboard
+                inputTab={InputTab.oneByOne}
+                buttonEffect={true}
+                buttonEffectCallback={() => {}}
+                onEnterClick={() => {}}
+                userInputSubmissionCallback={() => {}}
+                disabledKeysArray={["E", "L", "S"]}
+                gameState={GameState.gameStarted}
+                gameResult={GameResult.default}
+              />
             </div>
-            <p className="leading-6 text-left">Other special characters, not really!</p>
           </RulesModalSlide>
           <RulesModalSlide index={5}>
             <p className="leading-6 text-left">All clues based on teams from the following leagues.</p>
-            <div className="w-full flex flex-col items-center border-2 border-gray50 rounded-md p-2">
-              <WhiteSquaresContainer matcherText="Vfl Bochum 1848" userSubmissionArray={['v', 'f', 'l', 'b', 'o', 'c', 'h', 'u', 'm', '1', '8', '4', '8']}
-                                     gameState={GameState.gameOver} gameResult={GameResult.win}/>
-              <InputSection inputTab={InputTab.goForGlory} userInput="e" userNuclearInput={['V', 'f', 'l', ' ', 'B', 'o', 'c', 'h', 'u', 'm', ' ', '1', '8', '4', '8']} onClickTab={() => {
-              }}/>
+            <div>
+              <ul className="list-disc text-left text-lg">
+                <li className="">Premier League</li>
+                <li className="">Championship</li>
+                <li className="">League One</li>
+                <li className="">League Two</li>
+                <li className="">National League</li>
+                <li className="">Scottish Premiership</li>
+                <li className="">French Ligue 1</li>
+                <li className="">German Bundesliga</li>
+                <li className="">Italian Serie A</li>
+                <li className="">Spanish La Liga</li>
+                <li className="">American MLS</li>
+              </ul>
             </div>
-            <p className="leading-6 text-left">Other special characters, not really!</p>
+            <p className="leading-6 text-left">More being added in future. All data based on the BBC or official club/league websites.</p>
           </RulesModalSlide>
         </Carousel>
       </div>
