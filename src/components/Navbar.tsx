cd71dfa87c49ccd276e58cf31668533bc9bad2b8
bibhashdash <b.dash@12os.co.uk>
2023-11-11 21:48:47 +0000

    collapsible menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import {ClipboardIcon} from "@/icons/ClipboardIcon";
 import {RefreshIcon} from "@/icons/RefreshIcon";
 import {HandHeartIcon} from "@/icons/HandHeartIcon";
+import {useState} from "react";
+import {MenuIcon} from "@/icons/MenuIcon";
+import {CloseIcon} from "@/components/CloseIcon";
 
 interface NavbarProps {
   clickRulesIcon: () => void,
@@ -9,6 +12,7 @@ interface NavbarProps {
 }
 
 export const Navbar = ({clickRulesIcon, clickRefreshIcon, clickCreditsIcon}: NavbarProps) => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   return (
     <div className="bg-black300 w-full md:py-2 px-2 md:px-6 flex flex-col gap-2 items-center">
       <div>
@@ -18,18 +22,26 @@ export const Navbar = ({clickRulesIcon, clickRefreshIcon, clickCreditsIcon}: Nav
         <div className="flex flex-col items-center gap-2">
           <h1 className="text-sm sm:text-2xl text-white100 font-display">?ERFECT</h1>
         </div>
-        <div className="flex gap-4 md:gap-8">
-          <div onClick={() => clickRulesIcon()} className="cursor-pointer flex flex-col items-center focus:animate-button-pressed">
-            <ClipboardIcon size={24} color="rgba(176,176,176,0.46)" />
-            <p className="text-black50 text-xs font-display">Rules</p>
+        <div className="flex">
+          <div onClick={() => setShowMenu(true)} className={`cursor-pointer text-white100 text-xs sm:text-xl px-2 md:px-4 ${showMenu && 'hidden'}`}>
+            <MenuIcon size={20} color="#f8f8f8" />
           </div>
-          <div onClick={() => clickRefreshIcon()} className="cursor-pointer flex flex-col items-center focus:animate-button-pressed">
-            <RefreshIcon size={24} color="rgba(176,176,176,0.46)" />
-            <p className="text-black50 text-xs font-display">New Game</p>
+          <div className={`gap-4 md:gap-8 ${!showMenu && 'hidden'} flex border-2 border-gray50 px-2 md:px-4 rounded`}>
+            <div onClick={() => clickRulesIcon()} className="cursor-pointer flex items-center focus:animate-button-pressed">
+              {/*<ClipboardIcon size={24} color="rgba(176,176,176,0.46)" />*/}
+              <p className="text-black50 text-xs sm:text-xl hover:text-blue500">Rules</p>
+            </div>
+            <div onClick={() => clickRefreshIcon()} className="cursor-pointer flex items-center focus:animate-button-pressed">
+              {/*<RefreshIcon size={24} color="rgba(176,176,176,0.46)" />*/}
+              <p className="text-black50 text-xs sm:text-xl hover:text-blue500">New Game</p>
+            </div>
+            <div onClick={() => clickCreditsIcon()} className="cursor-pointer flex items-center focus:animate-button-pressed">
+              {/*<HandHeartIcon size={24} color="rgba(176,176,176,0.46)" />*/}
+              <p className="text-black50 text-xs sm:text-xl hover:text-blue500">Credits</p>
+            </div>
           </div>
-          <div onClick={() => clickCreditsIcon()} className="cursor-pointer flex flex-col items-center focus:animate-button-pressed">
-            <HandHeartIcon size={24} color="rgba(176,176,176,0.46)" />
-            <p className="text-black50 text-xs font-display">Credits</p>
+          <div onClick={() => setShowMenu(false)} className={`${!showMenu && 'hidden'} text-white100 text-xs sm:text-xl cursor-pointer flex items-center`}>
+            <CloseIcon size={20} color="#f8f8f8" />
           </div>
         </div>
       </div>
