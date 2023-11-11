81ae91e19e6f1191c0ec5094a2c71fd068f2b332
bibhashdash <b.dash@12os.co.uk>
2023-11-11 17:10:27 +0000

    adding credit names

--- src/components/CreditsModal.tsx
+++ src/components/CreditsModal.tsx
@@ -5,13 +5,38 @@ interface CreditsModalProps {
   onClickClose: () => void;
 }
 
-export const CreditsModal = ({onClickClose}:CreditsModalProps) => {
-   return (
-     <div className="h-screen w-full py-2 lg:py-6 text-white100 flex flex-col">
-       <div className="flex justify-between w-full my-5 px-2">
-         <h1 className="text-white100">Credits</h1>
-         <CloseIcon onClick={onClickClose} color="#f8f8f8" size={28}/>
-       </div>
-     </div>
-   )
+export const CreditsModal = ({onClickClose}: CreditsModalProps) => {
+  return (
+    <div className="h-screen w-full py-2 lg:py-6 text-white100 flex flex-col">
+      <div className="flex justify-between w-full my-5 px-2">
+        <h1 className="text-white100">Credits</h1>
+        <CloseIcon onClick={onClickClose} color="#f8f8f8" size={28}/>
+      </div>
+      <div className="text-xs text-center px-2 md:px-8 flex flex-col gap-4 md:gap-10">
+        <p className="text-left leading-4 md:text-center">
+          As the game developer, I'd like to thank the following folks for their time, advice, guidance, and (most
+          importantly!) feedback.
+        </p>
+        <div className="w-full flex justify-center text-xl">
+          <ul className="list-disc text-left">
+            <li>Kirstie Dash</li>
+            <li>Arjun Dash</li>
+            <li>Nathaniel Dash</li>
+            <li>Shishir Dash</li>
+            <li>Gary Rose</li>
+            <li>Paul Jervis</li>
+            <li>Julius Yam</li>
+            <li>Craig Pickles</li>
+            <li>Andy Gibson</li>
+            <li>Felix Wentworth</li>
+          </ul>
+        </div>
+        <p className="text-left leading-4 md:text-center">
+          If you'd like to make any suggestions or provide any feedback, I'd love to hear from you! Please feel free to
+          connect with me on <a className="text-blue500"
+                                href="https://www.linkedin.com/in/bibhash-dash-57307921/" target="_blank">LinkedIn</a>.
+        </p>
+      </div>
+    </div>
+  )
 }
