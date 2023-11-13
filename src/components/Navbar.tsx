a75d31449c75204f58747cca920f56c114f9ce48
bibhashdash <b.dash@12os.co.uk>
2023-11-13 19:18:33 +0000

    hamburger menu refinements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,26 +22,29 @@ export const Navbar = ({clickRulesIcon, clickRefreshIcon, clickCreditsIcon}: Nav
         <div className="flex flex-col items-center gap-2">
           <h1 className="text-sm sm:text-2xl text-white100 font-display">?ERFECT</h1>
         </div>
-        <div className="flex">
-          <div onClick={() => setShowMenu(true)} className={`cursor-pointer text-white100 text-xs sm:text-xl px-2 md:px-4 ${showMenu && 'hidden'}`}>
-            <MenuIcon size={20} color="#f8f8f8" />
-          </div>
-          <div className={`gap-4 md:gap-8 ${!showMenu && 'hidden'} flex border-2 border-gray50 px-2 md:px-4 rounded`}>
-            <div onClick={() => clickRulesIcon()} className="cursor-pointer flex items-center focus:animate-button-pressed">
-              {/*<ClipboardIcon size={24} color="rgba(176,176,176,0.46)" />*/}
+
+        <div className="flex border-2 border-gray50 rounded">
+          <div className={`gap-4 md:gap-8 ${!showMenu && 'hidden'} flex px-2 md:px-4 rounded`}>
+            <div onClick={() => clickRulesIcon()}
+                 className="cursor-pointer flex items-center focus:animate-button-pressed">
               <p className="text-black50 text-xs sm:text-xl hover:text-blue500">Rules</p>
             </div>
-            <div onClick={() => clickRefreshIcon()} className="cursor-pointer flex items-center focus:animate-button-pressed">
-              {/*<RefreshIcon size={24} color="rgba(176,176,176,0.46)" />*/}
+            <div onClick={() => clickRefreshIcon()}
+                 className="cursor-pointer flex items-center focus:animate-button-pressed">
               <p className="text-black50 text-xs sm:text-xl hover:text-blue500">New Game</p>
             </div>
-            <div onClick={() => clickCreditsIcon()} className="cursor-pointer flex items-center focus:animate-button-pressed">
-              {/*<HandHeartIcon size={24} color="rgba(176,176,176,0.46)" />*/}
+            <div onClick={() => clickCreditsIcon()}
+                 className="cursor-pointer flex items-center focus:animate-button-pressed">
               <p className="text-black50 text-xs sm:text-xl hover:text-blue500">Credits</p>
             </div>
           </div>
-          <div onClick={() => setShowMenu(false)} className={`${!showMenu && 'hidden'} text-white100 text-xs sm:text-xl cursor-pointer flex items-center`}>
-            <CloseIcon size={20} color="#f8f8f8" />
+          <div onClick={() => setShowMenu(true)}
+               className={`cursor-pointer text-white100 text-xs sm:text-xl px-2 md:px-4 ${showMenu && 'hidden'}`}>
+            <MenuIcon size={24} color="#f8f8f8"/>
+          </div>
+          <div onClick={() => setShowMenu(false)}
+               className={`${!showMenu && 'hidden'} px-2 md:px-4 text-white100 text-xs sm:text-xl cursor-pointer flex items-center`}>
+            <CloseIcon size={24} color="#f8f8f8"/>
           </div>
         </div>
       </div>
