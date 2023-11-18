7c759430284a031ddcde1ec7617e982384b67a34
bibhashdash <b.dash@12os.co.uk>
2023-11-18 21:07:23 +0000

    fixing navbar positioning

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,11 +19,13 @@ export const Navbar = ({clickRulesIcon, elapsedSeconds, elapsedMinutes, clickRef
   return (
     <div className="bg-black300 w-full md:py-2 px-2 md:px-6 flex flex-col gap-2 items-center">
       <div className="flex items-center w-full justify-between">
-        <div className="flex flex-col items-center gap-2">
+        <div className="flex flex-col items-center justify-center">
           <h1 className="text-xs sm:text-2xl text-white100 font-display">?ERFECT</h1>
         </div>
-        <GameTimer gameState={gameState} elapsedSeconds={elapsedSeconds} elapsedMinutes={elapsedMinutes}/>
-        <div className="flex border-2 border-gray50 rounded">
+        <div className="">
+          <GameTimer gameState={gameState} elapsedSeconds={elapsedSeconds} elapsedMinutes={elapsedMinutes}/>
+        </div>
+        <div className="flex border-2 border-gray50 rounded z-20">
           <div className={`gap-4 md:gap-8 ${!showMenu && 'hidden'} flex px-2 md:px-4 rounded`}>
             <div onClick={() => {clickRulesIcon(); setShowMenu(false)}}
                  className="cursor-pointer flex items-center focus:animate-button-pressed">
