a0aaf9b5f8f2cbf7a9782c871a724c339c4b35e7
bibhashdash <b.dash@12os.co.uk>
2023-11-26 21:40:15 +0000

    some styling to main menu

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -3,6 +3,7 @@ import { styled, alpha } from '@mui/material/styles';
 import Menu, { MenuProps } from '@mui/material/Menu';
 import {Button, MenuItem} from "@mui/material";
 import {MenuIcon} from "@/icons/MenuIcon";
+import {CloseIcon} from "@/components/CloseIcon";
 const StyledMenu = styled((props: MenuProps) => (
   <Menu
     elevation={0}
@@ -16,13 +17,13 @@ const StyledMenu = styled((props: MenuProps) => (
     }}
     {...props}
   />
-))(({ theme }) => ({
+))(() => ({
   '& .MuiPaper-root': {
     borderRadius: 6,
-    marginTop: theme.spacing(1),
+    border: "2px solid #4d4d4d",
     minWidth: 180,
-    color:
-      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    color: "#f8f8f8",
+    backgroundColor: "#2d2d2d",
     boxShadow:
       'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
     '& .MuiMenu-list': {
@@ -31,16 +32,11 @@ const StyledMenu = styled((props: MenuProps) => (
     '& .MuiMenuItem-root': {
       '& .MuiSvgIcon-root': {
         fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity,
-        ),
       },
     },
+    '& .MuiMenuItem-root:hover': {
+      backgroundColor: "#0079cb"
+    }
   },
 }));
 
@@ -69,7 +65,7 @@ export const MainMenu = ({clickRulesIcon, clickRefreshIcon, clickCreditsIcon}: M
         aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
       >
-        <MenuIcon size={28} color={'#f8f8f8'}/>
+        { !open ? <MenuIcon size={28} color={'#f8f8f8'}/> : <CloseIcon color={'#f8f8f8'} size={28} /> }
       </Button>
       <StyledMenu
         id="basic-menu"
