063a480d7a044b2553a81d5595025fd6ce31f046
bibhashdash <b.dash@12os.co.uk>
2023-11-27 16:29:37 +0000

    fixing build errors

--- src/components/Speedometer.tsx
+++ src/components/Speedometer.tsx
@@ -1,14 +1,14 @@
 import {buildStyles, CircularProgressbarWithChildren} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
-const ProgressProvider = ({valueStart, valueEnd, children}) => {
-  const [value, setValue] = useState(valueStart);
+const ProgressProvider = ({valueStart, valueEnd, children}: {valueStart: number, valueEnd: number, children:any}) => {
+  const [value, setValue] = useState<number>(valueStart);
   useEffect(() => {
     setValue(valueEnd);
   }, [valueEnd]);
 
-  return children(value);
+  return children(value as number);
 };
 export default ProgressProvider;
 
@@ -19,14 +19,12 @@ export interface SpeedometerProps {
 }
 
 export const Speedometer = ({maxSpeed, speed}: SpeedometerProps) => {
-  const [pointerRotation, setPointerRotation] = useState<number>(-90);
-
   return (
     <div className="relative w-[200px] h-[200px]">
       <div className="absolute">
         <ProgressProvider valueStart={0} valueEnd={speed}>
           {
-            value => <CircularProgressbarWithChildren
+            (value: number) => <CircularProgressbarWithChildren
               value={value}
               maxValue={maxSpeed}
               circleRatio={0.5}
@@ -38,9 +36,6 @@ export const Speedometer = ({maxSpeed, speed}: SpeedometerProps) => {
                   strokeLinecap: "butt",
                   textColor: "#f8f8f8",
                   trailColor: "#f8f8f8",
-                  path: {
-                    transition: '0.15s'
-                  }
                 },
 
               )}
@@ -56,7 +51,7 @@ export const Speedometer = ({maxSpeed, speed}: SpeedometerProps) => {
         className="flex flex-col w-full items-center w-fit absolute top-[50%] -translate-y-[80%] left-[50%] -translate-x-[50%] border-b-4 border-blue500 rounded">
         <ProgressProvider valueStart={0} valueEnd={speed}>
           {
-            value => <div className="w-[2px] h-[50px] bg-white50 origin-bottom transition-transform duration-200"
+            (value: number) => <div className="w-[2px] h-[50px] bg-white50 origin-bottom transition-transform duration-200"
                           style={{
                             transform: `rotate(${((value / maxSpeed) * 180) - 90}deg)`,
 
