22c85f9892f4854a6c1d5b30e7887f034503be5a
bibhashdash <b.dash@12os.co.uk>
2023-11-28 13:13:11 +0000

    fixing storybook build errors

--- src/components/ScoreModal.tsx
+++ src/components/ScoreModal.tsx
@@ -1,16 +1,8 @@
 import {CloseIcon} from "./CloseIcon";
 import React, {useEffect, useState} from "react";
-import {GameTimer} from "./GameTimer";
-import {FootballIcon} from "../icons/FootballIcon";
-import {DividerLine} from "./DividerLine";
-import {MedalIcon} from "../icons/MedalIcon";
-import {ScoreBreakdown} from "../utlities/models";
-import {useApiService} from "../services/apiService";
 import {Speedometer} from "../components/Speedometer";
-import Lottie from "lottie-react";
-import fireAnimation from "../lottie/fireAnime.json"
-import {useGameControlContext} from "@/contexts/gamecontrol";
-import {LivesLostChart} from "@/components/LivesLostChart";
+import {useGameControlContext} from "../contexts/gamecontrol";
+import {LivesLostChart} from "../components/LivesLostChart";
 
 interface ScoreModalProps {
   onClickClose: () => void;
@@ -51,13 +43,8 @@ export const ScoreModal = ({onClickClose}:ScoreModalProps) => {
         <h1 className="text-white100 font-display">Your Score</h1>
         <CloseIcon onClick={onClickClose} color="#f8f8f8" size={28}/>
       </div>
-      <div className="flex justify-evenly">
+      <div className="flex">
         <Speedometer speed={scoreBreakdown.timeScore} maxSpeed={fastestKnownTime} />
-        {/*<div className="flex flex-col items-center">*/}
-        {/*  <div className="w-full">*/}
-        {/*    <Lottie loop={false} style={{height: '75px', width: '100px'}} animationData={fireAnimation} />*/}
-        {/*  </div>*/}
-        {/*</div>*/}
       </div>
       <LivesLostChart livesLost={7 - scoreBreakdown.livesBonus} />
 
