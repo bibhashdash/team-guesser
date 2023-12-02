9a616847708c32d406748c27650405342a123df8
Bibhash Dash <63647219+bibhashdash@users.noreply.github.com>
2023-12-02 14:32:41 +0000

    Pwa conversion (#20)
    * wip...pwa
    
    * pwa attempt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+    // ... other options you like
+});
 
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  // next config
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
